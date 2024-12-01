6eb148f23fce4e0c83a70261e8c03b9b332fd726
Iosif245 <grosu_iosif@yahoo.com>
2024-12-02 00:46:38 +0200

    getAllUsers

--- src/app/components/get-all-users/get-all-users.component.ts
+++ src/app/components/get-all-users/get-all-users.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../../models/user';
+import { UserService } from '../../services/user-service/user.service';
 
 @Component({
   selector: 'app-get-all-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './get-all-users.component.html',
   styleUrl: './get-all-users.component.css',
 })
-export class GetAllUsersComponent {}
+export class GetAllUsersComponent implements OnInit {
+  users: IUser[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe((users) => {
+      this.users = users;
+    });
+  }
+}
