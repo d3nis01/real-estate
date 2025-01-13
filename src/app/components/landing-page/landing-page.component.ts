4c9731cd65def1a6fb80abaef4762402b27ad63a
d3nis <denis.deaconu2003@gmail.com>
2025-01-13 16:11:37 +0200

    Improved landing page

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [MatButtonModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css',
+  styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  constructor(private router: Router) {}
+
+  goToListings(): void {
+    this.router.navigate(['/get-all-listings']);
+  }
+}
